8c65feba65fce7f50067b7f42374172ad1a910b7
mtkramer <kramerm1@mail.usf.edu>
2020-05-18 18:22:13 -0400

    part 1 complete

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Best Pics';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://www.naturesbestphotography.com/galleries/images/15wsr/84_DomenicoRoscigno.jpg';
+  image3 = 'https://i.ytimg.com/vi/FcRJVh6_yGs/maxresdefault.jpg';
 
   constructor() { }
 
