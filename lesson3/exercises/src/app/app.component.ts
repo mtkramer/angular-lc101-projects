ff15b74a1247c1f96e777f81e429f5041316416b
mtkramer <kramerm1@mail.usf.edu>
2020-06-03 09:21:18 -0400

    shuttle landing completed

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,7 +12,6 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
-  inFlight = false;
 
   launch(): void {
     if (this.color !== 'blue') {
@@ -25,6 +24,18 @@ export class AppComponent {
     else {
       window.alert("Shuttle is already in flight.\nShuttle must land before another lift off.")
     }
+  }
+
+  land(): void {
+    if (this.color === 'blue') {
+      window.alert("Shuttle is landing.\nLanding gear engaged.");
+      this.color = 'green';
+      this.height = 0;
+    }
+    else {
+      window.alert("Shuttle is not in flight.\nShuttle must lift off before it can land.");
+    }
+
   }
 
 }
